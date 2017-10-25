caa1a25b5ab775791800a82dbee236a2b97dcfa4
Sidney NiBo <29949873+snibo13@users.noreply.github.com>
2017-10-24 20:19:06 -0500

    footer php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html>
+  <footer class="w3-container w3-blue w3-center w3-margin-top">
+    <h4>the Socials</h4>
+    <a href = "https://www.instagram.com/snibo" target = "_blank" style = "border: 13px;"><i class="fa fa-instagram fa-2x w3-hover-opacity space"></i></a>
+    <a href = "https://www.twitter.com/snibo13" target = "_blank" style = "border: 13px;"><i class="fa fa-twitter fa-2x w3-hover-opacity space"></i></a>
+    <a href = "https://www.linkedin.com/in/snibo" target = "_blank" style = "border: 13px;"><i class="fa fa-linkedin fa-2x w3-hover-opacity space"></i></a>
+    <a href = "https://github.com/snibo13" target = "_blank" style = "border: 13px;"><i class = "fa fa-github fa-2x w3-hover-opacity space"></i></a>
+  </footer>
+</html>
