f340d77eaf7504d1885b96087052982ac6bf0af8
Sidney NiBo <snibo13@gmail.com>
2019-12-21 00:53:43 -0600

    Update to December 2019 Preperation
    Removed deprecated files, uploaded cartoon head and prepared to push new site.

--- autoscroll.js
+++
@@ -1,8 +0,0 @@
-$('html, body').animate(
-  {
-    scrollTop: $(document).height() - $(window).height()
-
-  }, 10000, function() {
-    $(this).animate({ scrollTop: 0 }, 1);
-}
-);
--- visibility.js
+++
@@ -1,8 +0,0 @@
-function visibility(ID) {
-  var x = document.getElementById(ID);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
